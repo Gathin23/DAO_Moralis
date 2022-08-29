24120fdd65ea602897513471b2703eda07391a02
Gathin <gathint23@gmail.com>
2022-08-29 22:16:49 +0530

    Inital smartcontract setup

---
+++ smartcontracts/contracts/Dao.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+interface IdaoContract {
+    function balanceOf(address, uint256) external view returns (uint256);
+}
+
+contract MoralisDao {
+
+    address public owner;
+    uint256 nextProposal;
+    uint256[] public validTokens;
+    IdaoContract daoContract;
+
+    constructor() {
+        owner = msg.sender;
+        nextProposal = 1;
+        daoContract = IdaoContract(0x2953399124F0cBB46d2CbACD8A89cF0599974963);
+        validTokens = [86917482796469741037850474189125837810770818944972258581609733706236968304641];
+    }
+}
