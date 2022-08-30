71d1287e38d28266f3f4d2c716a3dde48605396c
Gathin <gathint23@gmail.com>
2022-08-30 16:19:13 +0530

    Created proposal struct and createproposal func

--- smartcontracts/contracts/Dao.sol
+++ smartcontracts/contracts/Dao.sol
@@ -9,7 +9,7 @@ interface IdaoContract {
 contract MoralisDao {
 
     address public owner;
-    uint256 nextProposal;
+    uint256 nextProposal; 
     uint256[] public validTokens;
     IdaoContract daoContract;
 
@@ -18,5 +18,72 @@ contract MoralisDao {
         nextProposal = 1;
         daoContract = IdaoContract(0x2953399124F0cBB46d2CbACD8A89cF0599974963);
         validTokens = [86917482796469741037850474189125837810770818944972258581609733706236968304641];
+    }
+
+    struct proposal{
+        uint256 id;
+        bool exists;
+        string description;
+        uint deadline;
+        uint256 votesUp;
+        uint256 votesDown;
+        address[] canVote;
+        uint256 maxVotes;
+        mapping(address => bool) voteStatus;
+        bool countConducted;
+        bool passed;
+    }
+
+    mapping(uint256 => proposal) public Proposals;
+
+    event propsalCreated(
+        uint256 id,
+        string description,
+        uint256 maxVotes,
+        address proposer
+    );
+
+    event newVote(
+        uint256 votesUp,
+        uint256 votesDown,
+        address voter,
+        uint256 proposal,
+        bool votedFor
+    );
+
+    event poposalCount(
+        uint256 id,
+        bool passsed
+    );
+
+    //Private functions to check eligibility
+    function checkProposalEligibility(address _proposalist) private view returns (bool) {
+        for(uint256 i = 0; i < validTokens.length; i++){
+            if(daoContract.balanceOf(_proposalist, validTokens[i]) >= 1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function checkVoteEligibility(uint256 _id ,address _voter) private view returns(bool) {
+        for(uint256 i = 0; i < Proposals[_id].canVote.length; i++){
+            if(Proposals[_id].canVote[i] == _voter){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function createProposal(string memory _description, address[] memory _canVote) public {
+        require(checkProposalEligibility(msg.sender),"Only NFT holders can put forth Proposals");
+
+        proposal storage newProposal = Proposals[nextProposal];
+        newProposal.id = nextProposal;
+        newProposal.exists = true;
+        newProposal.description = _description;
+        newProposal.deadline = block.number + 100;
+        newProposal.canVote = _canVote;
+        newProposal.maxVotes = _canVote.length;
     }
 }
