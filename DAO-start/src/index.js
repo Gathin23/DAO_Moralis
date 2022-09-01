96a66bb1621ff7153aad2bda9ff82dfe279c0fb1
Gathin <gathint23@gmail.com>
2022-09-01 17:22:34 +0530

    Home page

--- DAO-start/src/index.js
+++ DAO-start/src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxx" serverUrl="xxx">
+    <MoralisProvider appId="aAxWeaqtHfBphQLwrAAn3CMJf1Sx4kafLZK3ERUx" serverUrl="https://lpwdvkzknjyw.usemoralis.com:2053/server">
       <BrowserRouter>
           <App />
       </BrowserRouter>
